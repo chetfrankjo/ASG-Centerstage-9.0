8b4c6d974b0cbbbdb6cc69c0f68629d60aa2cd43
chetfrankjo <99208343+chetfrankjo@users.noreply.github.com>
2024-01-05 18:53:37 -0800

    Update RobotDriver.java

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/RobotDriver.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/RobotDriver.java
@@ -717,14 +717,14 @@ public class RobotDriver {
                 break;
             case RIGHT:
                 clawR.setPosition(0.5);
-                clawL.setPosition(0.33);
+                clawL.setPosition(0.36);
                 break;
             case BOTH:
                 clawL.setPosition(0.5);
                 clawR.setPosition(0.5);
                 break;
             case OPEN:
-                clawL.setPosition(0.33);
+                clawL.setPosition(0.36);
                 clawR.setPosition(0.7);
                 break;
             /*case PRIMED:
