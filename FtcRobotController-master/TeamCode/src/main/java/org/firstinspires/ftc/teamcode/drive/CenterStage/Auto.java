46398b19f505e8303761b1fe031db6bbd1719188
chetfrankjo <99208343+chetfrankjo@users.noreply.github.com>
2024-02-19 22:15:07 -0800

    Update Auto.java

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/CenterStage/Auto.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/CenterStage/Auto.java
@@ -265,7 +265,10 @@ public class Auto extends LinearOpMode {
                         if (tagDetected && timer.time() < 2 && opModeIsActive()) {
                             double Xcurrent_error = Xpos+offpos-driver.getCurrentPos().getX();
 
-                            driver.goToAnotherPosition(new Pose2d(Xcurrent_error, 0, driver.getIMUHeading()), 0, 0, 0.5, Math.signum(Xcurrent_error)*-90, 0.3, 1, false, 1);
+                            //driver.goToAnotherPosition(new Pose2d(Xcurrent_error, 0, driver.getCurrentPos().getHeading()), 0, 0, 0.5, Math.signum(Xcurrent_error)*-90, 0.3, 1, false, 1);
+
+
+                            driver.drive(-Xcurrent_error/8, -driver.getCurrentPos().getY()/8, driver.getCurrentPos().getHeading()/8, false);
 
                         }
 
