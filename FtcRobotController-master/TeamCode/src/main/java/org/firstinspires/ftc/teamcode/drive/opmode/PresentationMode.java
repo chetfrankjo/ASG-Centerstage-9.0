153bdd80ea104941aede6f6f08d7cee7dd222674
Calegomo <caleslaw@outlook.com>
2024-03-09 00:45:41 -0800

    s

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/PresentationMode.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/PresentationMode.java
@@ -10,31 +10,46 @@ import org.firstinspires.ftc.teamcode.drive.RobotDriver;
 
 @TeleOp
 public class PresentationMode extends LinearOpMode {
+    boolean l = true;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         RobotDriver driver = new RobotDriver(hardwareMap, false);
+        driver.resetSlidesEncoder();
         driver.setDriveZeroPower(DcMotor.ZeroPowerBehavior.BRAKE);
         driver.setWeaponsState(General.WeaponsState.HOLDING);
         driver.setSlidesDisable(false);
 
 
+
         driver.setClawMode(General.ClawMode.BOTH);
-        driver.resetSlidesEncoder();
+
         driver.storeAll();
         driver.setSpeedyDeposit(true);
         driver.update();
 
+        driver.setSlidesDepositTarget(12);
+
         waitForStart();
-        driver.setWeaponsState(General.WeaponsState.EXTEND);
+
         while (opModeIsActive()) {
+
+            if (l) {
+                driver.resetSlidesEncoder();
+                driver.setWeaponsState(General.WeaponsState.EXTEND);
+            }
+            l = false;
             driver.update();
             driver.drive(0, 0, 0, false);
 
             if (driver.getFSRPressed()) {
                 driver.setWeaponsState(General.WeaponsState.DEPOSIT);
             }
+            telemetry.addData("slides pos", driver.getSlidesLength());
+            telemetry.update();
+
+            sleep(20);
 
         }
     }
